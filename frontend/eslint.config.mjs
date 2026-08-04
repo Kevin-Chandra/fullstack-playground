78ce465d0c290c2d362cc338f91beb0b4553380f
Kevin Chandra <kevinchandra0308@gmail.com>
2026-08-04 16:39:01 +0800

    Add linting package

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,3 +1,4 @@
+import stylistic from "@stylistic/eslint-plugin";
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
@@ -5,6 +6,14 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: { "@stylistic": stylistic },
+    rules: {
+      "@stylistic/indent": ["error", 2, { SwitchCase: 1 }],
+      "@stylistic/jsx-indent-props": ["error", 2],
+      "@stylistic/no-tabs": "error",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
