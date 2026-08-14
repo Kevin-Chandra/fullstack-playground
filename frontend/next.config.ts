8c2cfd8380bfdabc06c06f1c6a2b89543aea178c
Kevin Chandra <kevinchandra0308@gmail.com>
2026-08-14 17:53:14 +0800

    Update frontend config for staging deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output:
+    process.env.NEXT_OUTPUT_STANDALONE === "true" ? "standalone" : undefined,
+
   images: {
     remotePatterns: [
       {
